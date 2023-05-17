ebfe835fc38e1a8f7320b12676168f047a0f77dd
yusuf <kingshakzy@gmail.com>
2023-05-17 14:17:42 +0100

    Working map

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import './App.css'
-
+import './App.css';
+import "leaflet/dist/leaflet.css"
+import {Box, Text, Image, Flex,  } from "@chakra-ui/react";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 function App() {
-  
+
   return (
     <>
-    <div>Carriion</div>
+
+
+    <MapContainer center={[5.038963, 7.909470]} zoom={13}scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[5.038963, 7.909470]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+        
     </>
-  )
+  );
 }
 
 export default App
