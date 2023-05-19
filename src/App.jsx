058c2a71ba108f0e0c7e53a73ce9dc519acf67ab
yusuf <kingshakzy@gmail.com>
2023-05-19 13:48:23 +0100

    Music player UI design started

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import {BiTimeFive} from 'react-icons/bi';
 import { Clock } from './assets/minor-components/clock';
 import { Weather_Indicator } from './assets/minor-components/weather-widget';
 import { MapPage } from './assets/minor-components/mapPage';
-import { BsPlayFill } from 'react-icons/bs'
+import { BsFillPlayFill, BsPlayFill } from 'react-icons/bs';
+import { AiOutlineStepBackward, AiFillStepForward, AiOutlineStepForward } from 'react-icons/ai';
 function App() {
 
   return (
@@ -22,16 +23,19 @@ function App() {
         </Box>
       </Box> 
       <Flex w={'35vw'} h={'100vh'} borderLeftWidth={'1px'} borderColor={'gray.800'}>
-        <Box display={'flex'} flexDirection={'column'} w={'100%'} h={'10rem'} bg={'gray.800'} p={'4'}>
+        <Box display={'flex'} flexDirection={'column'} w={'100%'} h={'fit-content'} bg={'gray.600'} p={'4'}>
           <Box h={'fit-content'} display={'flex'} flexDirection={'row'}>
             <Image bg={'whiteAlpha.800'} w={'20'} alt='Fortune Image' h={'20'}>
             </Image>
-            <Box>
-              <Text color={'whiteAlpha.800'} fontSize={'18px'}>Hold My Hand</Text>
+            <Box px={'4'}>
+              <Text color={'whiteAlpha.800'} fontSize={'xl'}>Hold My Hand</Text>
+              <Text color={'whiteAlpha.800'} fontSize={'sm'}>Lady Gaga</Text>
             </Box>
           </Box>
-          <Box h={'auto'}>
-
+          <Box px={'8'} py={'6'} h={'auto'} display={'flex'} flexDirection={'row'} justifyContent={'space-between'}>
+              <AiOutlineStepBackward size={'28px'} style={{color: 'white'}}></AiOutlineStepBackward>
+              <BsFillPlayFill size={'32px'} style={{color: 'white'}}></BsFillPlayFill>
+              <AiOutlineStepForward size={'28px'} style={{color: 'white'}}></AiOutlineStepForward>
           </Box>
         </Box>
         
